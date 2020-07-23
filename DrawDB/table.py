a72ea0db421c4e2f2604ec0d39e2f66cb2360fba
Alexander Bespalov <magisterbes@gmail.com>
2020-07-23 22:29:24 +0300

    Colored edges

--- DrawDB/table.py
+++ DrawDB/table.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 table_pattern  = '''
    %n
     [
@@ -8,7 +10,10 @@ table_pattern  = '''
 row_pattern = ''' <tr><td port="{0}" border="1">{1}</td></tr> '''
 name_row_pattern = ''' <tr><td   bgcolor="red" border="1"><b>{0}</b></td></tr> '''
 key_row_pattern = ''' <tr><td  port="{0}"  border="1"><b>{1}</b></td></tr> '''
-link_pattern = '''{0}:{1} -> {2}:{3}'''
+link_pattern = '''{0}:{1} -> {2}:{3} [color="{4}"; penwidth=4]'''
+
+colors = ["#ff2222","#22ff22","#2222ff","#ff22ff","#ff5522","#22ffff","#aaaaaa",\
+           "#aa0000","#00aa00","#0000aa","#aa00aa","#aaaa00","#00aaaa",]
 
 class table:
 
@@ -33,10 +38,12 @@ class table:
         for field in self.fields:
             rows.append(row_pattern.format(field,field))
 
+        current_color = np.random.randint(0,len(colors)-1)
         for link in self.links:
             spl = link.split("-")
             links.append(link_pattern.format(spl[0].split(".")[0],spl[0].split(".")[1], \
-                                            spl[1].split(".")[0],spl[1].split(".")[1]))
+                                            spl[1].split(".")[0],spl[1].split(".")[1], \
+                                            colors[current_color]))
 
         result_table = table_pattern.replace("%n",self.name)
         result_table = result_table.replace("%s","\n".join(rows))
